7ba2446740b46d072b677517f5730176e900894d
Der Flatulator <derflatulator@gmail.com>
2013-03-20 16:20:23 +1100

    Added copyright

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ lessc ./css/main.less ./css/main.css
 github=0
 commit=0
 push=0
+contains_message=0
 
 while getopts "gcm:" opt; do
     case "$opt" in
@@ -16,29 +17,31 @@ while getopts "gcm:" opt; do
 		commit=1
         ;;
     m)  
+		contains_message=1
 		message=$OPTARG
         ;;
     esac
 done
 
-if [ $commit = 1 ] ; then
+if [ $commit -eq 1 ] ; then
 	echo "Preparing to commit..."
 	git add .
 	echo "Committing"
-    if [ $message = 1 ] ; then
-		git commit -m $message
+    if [ $contains_message -eq 1 ] ; then
+		#echo "git commit -m $message"
+		git commit -m "$message"
 	else
 		git commit
 	fi
 fi
 
 #post-commit
-if [ $push = 1 ] ; then
+if [ $push -eq 1 ] ; then
 	echo "Pushing FTP"
 	git ftp push -u chaost@derflatulator.com -p - ftp://derflatulator.com/lucasazzola.com/public_html/fb-friend-analytics
 fi
 
-if [ $github = 1 ] ; then
+if [ $github -eq 1 ] ; then
 	echo "Pushing to GitHub"
    	git push github master
 fi
